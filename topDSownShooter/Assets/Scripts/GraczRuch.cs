ce8a42b3a4b83b4c91e8c0ee3f4d04924912ab58
AnatolBanan <piotrstepniak10@gmail.com>
2019-09-21 14:42:44 +0200

    zrobiłem prawie ruch

---
+++ topDSownShooter/Assets/Scripts/GraczRuch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraczRuch : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector3 ruch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float prawaLewa = Input.GetAxis("Horizonmtal");
+        float goraDol = Input.GetAxis("Vertical");
+
+        ruch = new Vector3(prawaLewa, 0, goraDol) * predkosc;
+
+        transform.Translate(ruch);
+    }
+}
